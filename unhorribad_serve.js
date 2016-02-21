39e4e44946a4daa369b3c5cfedc676208a00231a
Vas <whiterocket@outlook.com>
2016-02-21 17:36:41 +0200

    fix weird bug that made nodejs drop headers, also add es6 featuers

--- unhorribad_serve.js
+++ unhorribad_serve.js
@@ -1,145 +1,132 @@
 #!/usr/bin/env node
-/* jshint esnext:true */
+// jshint esversion: 6
+// jshint asi: true
 
-"use strict";
+"use strict"
 
-const http = require("http");
-const fs = require("fs");
-const os = require("os");
-const path = require("path");
-const url = require("url");
-const zlib = require("zlib");
-const conf = {};
-
-function serve (res, conf) {
-	var gzip = zlib.createGzip();
-	conf.message["content-encoding"] = "gzip";
-
-	// we accept both streams and raw data
-	res.writeHead(conf.code, conf.message, conf.headers);
-	if (conf.data.constructor === fs.ReadStream) {
-		conf.data.pipe(gzip).pipe(res);
-	} else {
-		gzip.end(conf.data);
-		gzip.pipe(res);
-	}
-}
+const http = require("http")
+const fs = require("fs")
+const os = require("os")
+const path = require("path")
+const url = require("url")
 
 function escapeHTML (unsafe) {
 	return unsafe
-	.replace("&", "&amp;")
-	.replace("<", "&lt;")
-	.replace(">", "&gt;")
-	.replace('"', "&quot;")
-	.replace("'", "&#039;");
+		.replace("&", "&amp;")
+		.replace("<", "&lt;")
+		.replace(">", "&gt;")
+		.replace('"', "&quot;")
+		.replace("'", "&#039;")
  }
 
-class Render {
-	static directory (pathname, files) {
-		var head = `<head><meta charset='utf-8'><title>${pathname}</title></head>`;
-		var list = "";
-		var escapedfile = "";
-		var link = "";
-		for (var i = 0, len = files.length; i < len; i++) {
-			link = encodeURI("/" + path.relative(conf.pwd, files[i]));
-			escapedfile = escapeHTML(path.basename(files[i]));
-			list += `<li><a href="${link}">${escapedfile}</a></li>`;
-		}
-		var body = `<body><h1>${pathname}</h1><ul>${list}</ul></body>`;
-		return `<!DOCTYPE html><html>${head}${body}</html>`;
+function render_directory (pathname, files) {
+	const head = `<head><meta charset='utf-8'><title>${pathname}</title></head>`
+	let list = ""
+	let escapedfile = ""
+	let link = ""
+	for (let i = 0, len = files.length; i < len; i++) {
+		link = encodeURI("/" + path.relative(".", files[i]))
+		escapedfile = escapeHTML(path.basename(files[i]))
+		list += `<li><a href="${link}">${escapedfile}</a></li>`
 	}
+	const body = `<body><h1>${pathname}</h1><ul>${list}</ul></body>`
+	return `<!DOCTYPE html><html>${head}${body}</html>`
 }
 
-class ResponseConf {
+function ResponseConf (code, headers, data) {
+	this.code = code
+	this.headers = headers
+	this.data = data
+}
+
+class View {
 	static code (num, msg) {
-		return {
-			code: num,
-			message: {"Content-type": "text/plain"},
-			data: msg ? msg : "" + num
-		};
+		return new ResponseConf(
+			num,
+			{"Content-Type": "text/plain"},
+			msg ? msg : "" + num
+		)
 	}
 
-	static file (pathname) {
-		return {
-			code: 200,
-			message: {
-				"Content-type": "application/octet_stream",
+	static file (pathname, size) {
+		return new ResponseConf(
+			200,
+			{ 
+				"Content-Type": "application/octet_stream",
+				"Content-Disposition": `attachment; filename=${encodeURIComponent(pathname)};`,
+				"Content-Length": size,
 			},
-			data: fs.createReadStream(pathname),
-		};
+			fs.createReadStream(pathname)
+		)
 	}
 
 	static directory (pathname, files) {
-		return {
-			code: 200,
-			message: {
-				"Content-type": "text/html",
-			},
-			data: Render.directory(pathname, files),
-		};
+		return new ResponseConf(
+			200,
+			{"Content-Type": "text/html"},
+			render_directory(pathname, files)
+		)
 	}
 }
 
-function request_listener(req, res) {
-	function DRY (conf) { serve (res, conf); }
-	req.url = url.parse(req.url, true);
-	var pathname = path.join(conf.pwd, decodeURI(req.url.pathname));
-	fs.stat(pathname, function (err, stats) {
-		if (err) {
-			DRY(ResponseConf.code(404));
-		} else if (stats.isDirectory()) {
-			request_directory(pathname, DRY);
-		} else if (stats.isFile()) {
-			DRY(ResponseConf.file(pathname));
-		} else {
-			DRY(ResponseConf.code(501));
-		}
-	});
-}
+class Controller {
+	constructor() {
+		this.server = http.createServer(this.request_listener.bind(this))
+		this.port = 8080
+		this.host = this.sensible_ip()
+		this.server.listen(this.port, this.host)
+	}
 
-function request_directory (pathname, callback) {
-	fs.readdir(pathname, function (err, files) {
-		if (err) {
-			return callback(ResponseConf.code(500));
-		}
-		for (var i = 0, len = files.length; i < len; i++) {
-			files[i] = path.join(pathname, files[i]);
-		}
-		callback(ResponseConf.directory(pathname, files));
-	});
-}
+	serve (res, conf) {
+		res.writeHead(conf.code, conf.headers)
+		if (conf.data.constructor === fs.ReadStream) conf.data.pipe(res)
+		else res.end(conf.data)
+	}
 
-function sensible_ip() {
-	var ifaces = os.networkInterfaces();
-	for (var iface in ifaces) {
-		if (!ifaces.hasOwnProperty(iface)) {
-			continue;
-		}
-		iface = ifaces[iface];
-		for (var i = 0; i < iface.length; i++) {
-			if (iface[i].family === "IPv4" && iface[i].internal === false) {
-				return iface[i].address;
+	request_listener(req, res) {
+		req.url = url.parse(req.url, true)
+		const pathname = path.join(".", decodeURI(req.url.pathname))
+		fs.stat(pathname, (err, stats) => {
+			if (err) {
+				this.serve(res, View.code(404))
+			} else if (stats.isDirectory()) {
+				this.request_directory(req, res, pathname)
+			} else if (stats.isFile()) {
+				this.serve(res, View.file(pathname, stats.size))
+			} else {
+				this.serve(res, View.code(501))
 			}
-		}
+		})
 	}
-	return "localhost";
-}
 
-function configure() {
-	conf.pwd = process.env.PWD ? process.env.PWD : process.exit();
-	conf.host = sensible_ip();
-	conf.port = 8000;
-}
+	request_directory (req, res, pathname) {
+		fs.readdir(pathname, (err, files) => {
+			if (err) {
+				return this.serve(res, View.code(500))
+			}
+			for (let i = 0, len = files.length; i < len; i++) {
+				files[i] = path.join(pathname, files[i])
+			}
+			this.serve(res, View.directory(pathname, files))
+		})
+	}
 
-function start_server () {
-	var server = http.createServer(request_listener);
-	server.listen(conf.port, conf.hostname);
+	sensible_ip() {
+		const ifaces = os.networkInterfaces()
+		for (let iface in ifaces) {
+			if (!ifaces.hasOwnProperty(iface)) continue
+			iface = ifaces[iface]
+			for (let i = 0; i < iface.length; i++)
+				if (iface[i].family === "IPv4" && iface[i].internal === false)
+					return iface[i].address
+		}
+		return "localhost"
+	}
 }
 
 function main () {
-	configure();
-	console.log(`Server running at \x1B[94mhttp://${conf.host}:${conf.port}\x1B[0m`);
-	start_server();
+	const server = new Controller()
+	console.log(`Server running at \x1B[94mhttp://${server.host}:${server.port}\x1B[0m`)
 }
 
-main();
+main()
